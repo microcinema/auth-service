ab051d2d8305603e66be8b815e56c821012df7ec
Artem Shein <tema34468@gmail.com>
2026-08-09 20:34:48 +0300

    chore: update @microcinema/contracts to version 1.0.6 and refactor gRPC server initialization in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,15 @@
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
-import { type MicroserviceOptions, Transport } from '@nestjs/microservices'
 
 import { AppModule } from './app.module'
-import type { AllConfigs } from './config'
+import { createGrpcServer } from './infrastructure/grpc/grpc.server'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
-	const configService = app.get(ConfigService<AllConfigs>)
+	const configService = app.get(ConfigService)
 
-	const host = configService.get('grpc.host', { infer: true })
-	const port = configService.get('grpc.port', { infer: true })
-
-	const url = `${host}:${port}`
-
-	app.connectMicroservice<MicroserviceOptions>({
-		transport: Transport.GRPC,
-		options: {
-			package: 'auth.v1',
-			protoPath: 'node_modules/@microcinema/contracts/proto/auth.proto',
-			url,
-			loader: {
-				keepCase: false,
-				longs: String,
-				enums: String,
-				defaults: true,
-				oneofs: true
-			}
-		}
-	})
+	createGrpcServer(app, configService)
 
 	await app.startAllMicroservices()
 	await app.init()
